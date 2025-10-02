c85afaf494e62d5bcbdb6e0aef508ebe5ff4e444
KirillNetesov <Pr023d@gmale.com>
2025-10-02 11:35:20 +0000

    Full backup: обновление всех файлов с локальной машины

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/kirill.netesov/', // 👈 важно
+  base: '/',
   plugins: [react()],
 })
